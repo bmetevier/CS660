fixed variance bug in environment

src/env.py
@@ -17,11 +17,11 @@ class Environment(object):
             raise ValueError("invalid mean and variance arrays")
         
         self.means = means
-        self.vars = variances
+        self.stdevs = np.sqrt(variances)
     
     @property
     def n_actions(self):
         return self.means.size
     
     def get_reward(self, action):
-        return np.random.normal(self.means[action], self.vars[action])        
+        return np.random.normal(self.means[action], self.stdevs[action])        

src/plotter.py
@@ -23,10 +23,11 @@ def get_axes(ys, n_rounds, logmsg=""):
     elif "logx" in logmsg:
         X = np.log(X)
     
-    embed()
     if "logy" in logmsg:
         #first two indices are zero
         Ys = [np.log(y[2:]) for y in ys]
         X = X[2:]
+    else:
+        Ys = ys
 
     return X, Ys